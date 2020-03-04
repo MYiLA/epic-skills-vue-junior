d8a08c834194bb02184e1811d2955678e8d75dbf
MYiLA <polus180@ya.ru>
2020-03-04 15:50:21 +0300

    fix

--- my-kitimder/src/App.js
+++ my-kitimder/src/App.js
@@ -5,6 +5,19 @@ import List from './List.js';
 
 
 class App extends Component {
+  state = {
+    cats: [
+      'https://images.app.goo.gl/9PbYM4XvhDYEwC5v9',
+      'https://images.app.goo.gl/3cx8ATQZocooRdfEA',
+      'https://focus.ua/storage/pub/images/2017/65042.jpg',
+      'https://g1.delphi.lv/images/pix/676x385/qOgzGluBhgU/kakis-koz-46290695.jpg',
+      'https://trendymen.ru/images/article1/126316/attachments/11.jpg',
+    ],
+    indexOfCurrentCat: 0,
+    rightColumn: [],
+    leftColumn: [],
+  }
+
   render() {
     return (
       <div
